0230e69fec99d10d306505e85944c3963a166c46
Sanjib <sanjib12sinha@gmail.com>
2020-12-28 06:41:21 +0530

    second commit

--- bin/all_loop_pattern_in_dart.dart
+++ bin/all_loop_pattern_in_dart.dart
@@ -1,21 +1,59 @@
 class AllLoopPatternInDart {
+  /// a for loop starts with a counter variable
+  /// then it checks the condition is true or false
+  /// here 0 is <= 5 until it meets that condition
+  /// so we can increase - counter++
+  ///
+  /// let us run the code by pressing control+F5
+  ///
   void learnForLoop() {
     for (var counter = 0; counter <= 5; counter++) {
       print(counter);
     }
   }
+
+  /// while loop and do-while are little bit different from for loop
+  /// at least struture wise they are different
+  /// in most cases, we can manage with the help of for loop
+  /// however, there is circumstance where we need while and do-while loop
+  /// we can easily increase or decrease value using
+  /// while and do-while loop
+  /// let us see how we can do that
+  ///
+  void increaseValueUsingWhileLoop() {
+    // counter variable
+    var counter = 0;
+    // let us check the condition
+    while (counter <= 5) {
+      print(counter);
+      // now we can increase the value of counter
+      // until it equals 5
+      counter++;
+    }
+    print('++++++++++++++++++==');
+  }
+
+  /// now we are going to decrase the value using
+  /// while loop
+  ///
+  void decreaseValueUsingWhileLoop() {
+    var counter = 5;
+    // now our counter starts with 5
+    // we'll decrease its value until it is equal 0
+    while (counter >= 0) {
+      // here counter decreases until it is greater than or equal 0
+      print(counter);
+      counter--;
+    }
+  }
 }
+
+///
 
 void main(List<String> args) {
   var anInstanceOfForLoop = AllLoopPatternInDart();
-  anInstanceOfForLoop.learnForLoop();
+  // anInstanceOfForLoop.learnForLoop();
+  var anInstanceOfWhileLoop = AllLoopPatternInDart();
+  anInstanceOfForLoop.increaseValueUsingWhileLoop();
+  anInstanceOfWhileLoop.decreaseValueUsingWhileLoop();
 }
-
-/**
- * a for loop starts with a counter variable
- * then it checks the condition is true or false
- * here 0 is <= 5 until it meets that condition
- * so we can increase - counter++
- * 
- * let us run the code by pressing control+F5
- */
