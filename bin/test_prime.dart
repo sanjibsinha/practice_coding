e40597c718bc7741e7c631f63e083c2ad580d29e
Sanjib <sanjib12sinha@gmail.com>
2020-12-30 09:02:34 +0530

    next commit

--- bin/test_prime.dart
+++ bin/test_prime.dart
@@ -1,43 +1,38 @@
-/// a prime number has two factors
+/// prime numbers are numbers that have only two factors
+/// the number itself, and 1
+///
+/// when a number more than two factors, it is called composite number
+/// based on that we can test a number whether a number is prime or not
+///
 ///
 class TestPrime {
   int start = 1;
   int end = 0;
   int factors = 0;
-
   void testPrime(int testPrime) {
     end = testPrime;
     for (start; start <= end; start++) {
       if (end % start == 0) {
         factors++;
-
-        /// we can refactor this code a little bit
-        /// we can get the number of factos
-        ///
-        print('Number of factors: $factors');
       }
     }
+    /** if (factors <= 2) {
+      print('The number $end is prime.');
+    } else {
+      print('The number $end is not prime.');
+    }
+    */
 
-    /// instead using if-else we can test the prime byusing ternary operators
+    /// we can refactor the above logic
     ///
     (factors <= 2)
-        ? print('The number $end is Prime')
-        : print('The number $end is not Prime');
+        ? print('The number $end is prime.')
+        : print('The number $end is not prime.');
+    ;
   }
 }
 
 void main(List<String> args) {
   var anInstance = TestPrime();
-
-  /// we have passed 7
-  /// let's run the code again
-  anInstance.testPrime(72);
+  anInstance.testPrime(7);
 }
-
-/**
- * Number of factors: 1
-Number of factors: 2
-The number 71 is Prime
-
-
- */
