c37f24e85735b56932c20c6a01b2e5c45b7b257b
Sanjib <sanjib12sinha@gmail.com>
2020-12-30 06:47:37 +0530

    next commit

--- bin/test_prime.dart
+++ bin/test_prime.dart
@@ -10,22 +10,34 @@ class TestPrime {
     for (start; start <= end; start++) {
       if (end % start == 0) {
         factors++;
+
+        /// we can refactor this code a little bit
+        /// we can get the number of factos
+        ///
+        print('Number of factors: $factors');
       }
     }
-    if (factors <= 2) {
-      print('The number is prime');
-    } else {
-      print('The number is not prime.');
-    }
+
+    /// instead using if-else we can test the prime byusing ternary operators
+    ///
+    (factors <= 2)
+        ? print('The number $end is Prime')
+        : print('The number $end is not Prime');
   }
 }
 
 void main(List<String> args) {
   var anInstance = TestPrime();
-  anInstance.testPrime(11);
+
+  /// we have passed 7
+  /// let's run the code again
+  anInstance.testPrime(72);
 }
 
 /**
- * The number is not prime.
-Exited
+ * Number of factors: 1
+Number of factors: 2
+The number 71 is Prime
+
+
  */
