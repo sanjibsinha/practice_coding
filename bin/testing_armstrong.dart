edf9553490bfc046d4eefdd018e2111e547f163c
Sanjib <sanjib12sinha@gmail.com>
2021-01-04 05:45:39 +0530

    armstrong numbers with definition

---
+++ bin/testing_armstrong.dart
@@ -0,0 +1,8 @@
+/// in dcimal system, that is in base 10, an armstrong number is like this:
+/// 1 to the power 1 is 1.
+/// 153 = 1 to the power 3 + 5 to the power 3 + 3 to the power 3
+/// So 1 and 153 is armstrong numbers
+///
+class TestingArmstrong {
+  int result = 0;
+}
