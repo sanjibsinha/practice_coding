682f474572e0e8c87f005a71e7d7939469d09f93
sanjib <sanjib12sinha@gmail.com>
2021-01-11 01:52:24 +0530

    added find hcf to new-fature

---
+++ bin/hcf_of_two_numbers.dart
@@ -0,0 +1,18 @@
+class HCFOfTwoNumbers {
+  int big, small, result;
+  int findHCF(int big, int small) {
+    while (big != small) {
+      if (big > small) {
+        big = big - small;
+      } else {
+        small = small - big;
+      }
+    }
+    return big;
+  }
+}
+
+void main(List<String> args) {
+  var o = HCFOfTwoNumbers();
+  print(o.findHCF(24, 36));
+}
