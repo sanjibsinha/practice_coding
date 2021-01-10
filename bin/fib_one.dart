28212f4572b37860a1f601d2e25e5ee9cdc7a29e
sanjib <sanjib12sinha@gmail.com>
2021-01-11 02:07:13 +0530

    added fib one

--- bin/fib_one.dart
+++ bin/fib_one.dart
@@ -1,10 +1,16 @@
 class FibOne {
+  int termOne = 0, termTwo = 1, nextTerm;
   void getFib(int n) {
-    for (var i = 0; i < n; i++) {}
+    for (var i = 1; i <= n; ++i) {
+      print(termOne);
+      nextTerm = termOne + termTwo;
+      termOne = termTwo;
+      termTwo = nextTerm;
+    }
   }
 }
 
 void main(List<String> args) {
   var o = FibOne();
-  o.getFib(4);
+  o.getFib(10);
 }
